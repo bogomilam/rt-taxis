51ff70cc6b39aa493c55b51d0746d3e71fe8d27c
bogomilam <bogomilmihaylov@live.com>
2020-09-22 20:29:31 +0100

    map and get drivers request, no lat or lng yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+// import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
 import './App.css';
+import API from "./adapters/API"
+
+const TAXI_ENDPOINT = "https://qa-interview-test.qa.splytech.io/api/drivers?"
+const CORS_URL = `https://cors-anywhere.herokuapp.com/${TAXI_ENDPOINT}`
+
 
 function Map() {
+  const [ drivers, setDrivers ] = useState(null)
+
+  const getTaxis = () => {
+    return fetch(CORS_URL).then(jsonify).then(data => setDrivers(data))
+  }
+
+  const jsonify = res => res.json()
+
+  useEffect(() => {
+    getTaxis()
+    .catch(console.error)
+  }, []);
+
+  
   return (
     <GoogleMap
     defaultZoom={14}
     defaultCenter={{ lat: 51.5049375, lng: -0.0964509}}
-   />
+    >
+      
+    </GoogleMap>
   );
 }
 
 const WrappedMap = withScriptjs(withGoogleMap(Map))
 
+
 export default function App() {
-return <div>
+  
+ 
+  
+return (
+<div>
   <WrappedMap 
-  googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAmtFOC7NcpCM4YwCEZTm4zpgpVtesPJMI"
+    googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAmtFOC7NcpCM4YwCEZTm4zpgpVtesPJMI"
     loadingElement={<div style={{ height: `100%` }} />}
     containerElement={<div style={{ height: `400px` }} />}
     mapElement={<div style={{ height: `100%` }} />}
-    />
+  >
+    
+  </WrappedMap>
 </div>
+)
 };
---
+++ src/adapters/API.js
@@ -0,0 +1,11 @@
+const TAXI_ENDPOINT = "https://qa-interview-test.qa.splytech.io/api/drivers?"
+const CORS_URL = `https://cors-anywhere.herokuapp.com/${TAXI_ENDPOINT}`
+const jsonify = res => res.json()
+
+const getTaxis = () => {
+    return fetch(CORS_URL).then(jsonify)
+}
+
+export default {
+    getTaxis
+}
