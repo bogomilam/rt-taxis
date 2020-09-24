eb9b4e0f909d3e6c93db3e6ae2754c86e14704ee
bogomilam <bogomilmihaylov@live.com>
2020-09-24 15:08:08 +0100

    adding pseudocoding

--- src/App.js
+++ src/App.js
@@ -4,24 +4,25 @@ import './App.css';
 import Header from "./components/Header"
 import {Slider} from "@material-ui/core"
 
-
-
 const TAXI_ENDPOINT = "https://qa-interview-test.qa.splytech.io/api/drivers?&longitude=-0.0964509&count=5"
 const CORS_URL = `https://cors-anywhere.herokuapp.com/${TAXI_ENDPOINT}`
 
 
-
-
 const Map = () => {
+  // live taxis from ENDPOINT
   const [ cabs, setCabs ] = useState([])
-  const [ centerPoint, setCenterPoint ] = useState([51.5049375, -0.0964509])
-  const [ distance, setDistance ] = useState(1)
+  // center point of map for the new coordinates
+  const [ centerPoint ] = useState([51.5049375, -0.0964509])
+  // current distance/radius in km
+  const [ distance ] = useState(1)
+  // radius of the earth in km
   const [ radius ] = useState(6371)
+  //angular distance for the new coordinates
   const [ angularDistance ] = useState(distance/radius)
-  // const [ newCoord, setNewCoord ] = useState([0, 0])
-  const [ newBearing, setNewBearing ] = useState([])
+  // const [ newBearings, setNewBearings ] = useState({})
 
 
+  // a function that takes a bearing and calculates the new coordinates using distance and center point from state
   const newPoints = (br) => {
     let newCoodArr = []
     let la2 = Math.asin( Math.sin(centerPoint[0] * Math.cos(angularDistance) + (Math.cos(centerPoint[0]) * Math.sin(angularDistance)) * Math.cos(br)) )
@@ -32,24 +33,31 @@ const Map = () => {
      return newCoodArr
   }
 
+  // fetch live taxi data
   const getTaxis = () => {
     return fetch(CORS_URL).then(jsonify).then(data => {
       setCabs(data.drivers)
     })
   }
 
+  // Format response into JSON
   const jsonify = res => res.json()
 
+  //Call for the taxi data once the app has been loaded
   useEffect(() => {
     getTaxis();
   }, []);
 
-  const newtaxiBearings = cabs.map(c => c.location.bearing).forEach(br => (
-    // console.log(newPoints(br)[0])
-     <Marker  position={{ lat: newPoints(br)[0], lng: newPoints(br)[1] }} />
-  ))
-    // console.log(newPoints(br)) )
-  // console.log(newtaxiBearings)
+
+// grab the bearing of each taxi and feed it to the new coordinates solution
+  const newtaxiBearings = cabs.map(c => c.location.bearing).forEach(br => {
+    //loggin each bearing of 
+    console.log(newPoints(br)[0], newPoints(br)[1] )
+     return <Marker  
+     position={{ lat: newPoints(br)[0], lng: newPoints(br)[1] }} 
+     />
+  })
+
 
 
   return (
@@ -57,25 +65,25 @@ const Map = () => {
     defaultZoom={14}
     defaultCenter={{ lat: 51.5049375, lng: -0.09612135008594562 }}
     >
-       <Marker position={{ lat: 51.5049375, lng: -0.0964509 }}/>
-
-
+      {cabs.map(c => c.location.bearing).forEach(br => {
+    
+    console.log(newPoints(br)[0], newPoints(br)[1] )
+     return <Marker  
+     position={{ lat: newPoints(br)[0], lng: newPoints(br)[1] }} 
+     />
+      } )}
+       {/* <Marker position={{ lat: 51.5049375, lng: -0.0964509 }}/> */}
 {/* {cabs.map(c => console.log(c.location.bearing))} */}
-
-
-
      
     </GoogleMap>
   )
   }
   
-
-
 const WrappedMap = withScriptjs(withGoogleMap(Map))
 
 export default function App() {
  
-  const mark = [
+  const marks = [
     {
       value: 0,
       label: "1"
@@ -93,17 +101,17 @@ export default function App() {
 return (
 <div>
   <Header />
-  <div style={{ width: 650, margin: 50 }} >
+<div style={{ width: 650, margin: 50 }} >
   <Slider 
-  defaultValue={10}
-  maxValue={100}
-  step={10}
-  onChange={getValue}
-  marks={mark}
-  valueLabelDisplay="auto"
+    defaultValue={10}
+    maxValue={100}
+    step={10}
+    onChange={getValue}
+    marks={marks}
+    valueLabelDisplay="auto"
   >
   </Slider>
-  </div>
+</div>
   <WrappedMap 
     googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAmtFOC7NcpCM4YwCEZTm4zpgpVtesPJMI"
     loadingElement={<div style={{ height: `100%` }} />}
